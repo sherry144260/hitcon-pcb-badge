ff836033044ec849b11ef9c6700e88ff57dfe4e6
Tony Yang <t510599@gmail.com>
2025-05-03 22:07:00 +0800

    feat: PacketProcessor.event_handler impl

--- backend/game_logic.py
+++ backend/game_logic.py
@@ -14,25 +14,31 @@ class GameLogic:
 
 
     # ===== APIs for PacketProcessor =====
+    @PacketProcessor.event_handler
     async def on_single_badge_activity_event(evt: SingleBadgeActivityEvent):
         pass
 
 
+    @PacketProcessor.event_handler
     async def on_two_badge_activity_event(evt: TwoBadgeActivityEvent):
         pass
 
 
+    @PacketProcessor.event_handler
     async def on_game_activity_event(evt: GameActivityEvent):
         pass
 
 
+    @PacketProcessor.event_handler
     async def on_sponsor_activity_event(evt: SponsorActivityEvent):
         pass
 
 
+    @PacketProcessor.event_handler
     async def on_proximity_event(evt: ProximityEvent):
         pass
 
 
+    @PacketProcessor.event_handler
     async def on_pub_announce_event(evt: PubAnnounceEvent):
         pass
