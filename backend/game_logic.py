965844a628b48ec34c585e6b3d520e18cfa0c12b
Chi-Feng Tsai <ktpss95112@gmail.com>
2025-05-07 11:42:40 +0800

    add game score

--- backend/game_logic.py
+++ backend/game_logic.py
@@ -3,11 +3,32 @@ from dataclasses import dataclass, fields
 from datetime import datetime, timedelta
 import pymongo
 
+try:
+    from enum import StrEnum
+except ImportError:
+    from enum import Enum
+    class StrEnum(str, Enum):
+        def __str__(self):
+            return str(self.value)
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}.{self.name}"
+
+
+class GameType(StrEnum):
+    SHAKE_BADGE = "shake_badge"
+    DINO = "dino"
+    SNAKE = "snake"
+    TETRIS = "tetris"
+    CONNECT_SPONSOR = "connect_sponsor"
+    RECTF = "rectf"
+
 
 @dataclass
 class Constants:
     DATABASE_NAME: str = "game_logic"
     ATTACK_HISTORY_COLLECTION: str = "attack_history"
+    SCORE_HISTORY_COLLECTION: str = "score_history"
 
     STATION_SCORE_LB: int = -1000
     STATION_SCORE_UB: int = 1000
@@ -49,13 +70,15 @@ class _GameLogic:
         # TODO: maybe use config to update constants
         self.db = mongo_client[const.DATABASE_NAME]
         self.attack_history = self.db[const.ATTACK_HISTORY_COLLECTION]
+        self.score_history = self.db[const.SCORE_HISTORY_COLLECTION]
 
         if start_time is None:
             start_time = datetime.now()
         self.start_time = start_time
 
-    async def clear_attack_history(self):
+    async def clear_database(self):
         await self.attack_history.delete_many({})
+        await self.score_history.delete_many({})
 
     async def attack_station(self, player_id: int, station_id: int, amount: int, timestamp: datetime):
         """
@@ -63,6 +86,7 @@ class _GameLogic:
         This will add a record in the database. The station's score will be calculated
         based on the history of attacks.
         """
+        # TODO: validate the player_id and the amount
         await self.attack_history.insert_one({
             "player_id": player_id,
             "station_id": station_id,
@@ -70,8 +94,7 @@ class _GameLogic:
             "timestamp": timestamp,
         })
 
-    async def get_station_score(self, station_id: int, before: datetime = None) -> int:
-        # TODO: cache the results
+    async def get_station_score_history(self, station_id: int, before: datetime = None):
         if before is None:
             before = datetime.now()
 
@@ -81,6 +104,14 @@ class _GameLogic:
                 "timestamp": {"$lt": before},
             }
         ).sort("timestamp", pymongo.ASCENDING)
+
+        async for record in cursor:
+            yield record
+
+    async def get_station_score(self, station_id: int, before: datetime = None) -> int:
+        # TODO: cache the results
+        if before is None:
+            before = datetime.now()
 
         total_score = 0
         time_pointer = self.start_time
@@ -93,27 +124,110 @@ class _GameLogic:
                 time_pointer += timedelta(seconds=const.STATION_SCORE_DECAY_INTERVAL)
                 total_score += -1 * sign(total_score) * min(const.STATION_SCORE_DECAY_AMOUNT, abs(total_score))
 
-        async for record in cursor:
+        async for record in self.get_station_score_history(station_id, before):
             proceed(record["timestamp"])
             total_score = clamp(total_score + record["amount"], const.STATION_SCORE_LB, const.STATION_SCORE_UB)
 
         proceed(before)
         return total_score
 
+    async def receive_game_score(self, player_id: int, station_id: int, score: int, game_type: GameType, timestamp: datetime):
+        match game_type:
+            case GameType.SHAKE_BADGE:
+                # TODO: validate the score and timestamp
+                pass
 
-async def test_station_score_history():
+            case GameType.DINO:
+                # TODO: validate the score and timestamp
+                pass
+
+            case GameType.SNAKE:
+                # TODO: validate the score and timestamp
+                pass
+
+            case GameType.TETRIS:
+                # TODO: validate the score and timestamp
+                pass
+
+            case GameType.CONNECT_SPONSOR:
+                # TODO: validate the score and timestamp
+                pass
+
+            case GameType.RECTF:
+                # TODO: validate the score and timestamp
+                pass
+
+        await self.score_history.insert_one({
+            "player_id": player_id,
+            "station_id": station_id,
+            "score": score,
+            "game_type": game_type,
+            "timestamp": timestamp,
+        })
+
+    async def get_game_history(self, *, player_id: int = None, station_id: int = None, game_type: GameType = None, before: datetime = None) -> int:
+        if before is None:
+            before = datetime.now()
+
+        query = {"timestamp": {"$lt": before}}
+
+        if player_id is not None:
+            query["player_id"] = player_id
+
+        if station_id is not None:
+            query["station_id"] = station_id
+
+        if game_type is not None:
+            query["game_type"] = game_type
+
+        cursor = self.score_history.find(query).sort("timestamp", pymongo.ASCENDING)
+
+        async for record in cursor:
+            yield record
+
+    async def get_game_score(self, *, player_id: int = None, station_id: int = None, game_type: GameType = None, before: datetime = None) -> int:
+        # TODO: cache the results
+        if before is None:
+            before = datetime.now()
+
+        query = {"timestamp": {"$lt": before}}
+
+        if player_id is not None:
+            query["player_id"] = player_id
+
+        if station_id is not None:
+            query["station_id"] = station_id
+
+        if game_type is not None:
+            query["game_type"] = game_type
+
+        cursor = await self.score_history.aggregate([
+            {"$match": query},
+            {"$group": {
+                "_id": None,
+                "total_score": {"$sum": "$score"},
+            }},
+        ])
+        result = await cursor.to_list(length=1)
+
+        return result[0]["total_score"] if result else 0
+
+
+async def test_attack_station_score_history():
     const.reset()
     const.STATION_SCORE_DECAY_INTERVAL = 1
     eps = 0.1
 
     time_base = datetime.now()
     gl = _GameLogic(pymongo.AsyncMongoClient("mongodb://localhost:27017"), time_base)
-    await gl.clear_attack_history()
+    await gl.clear_database()
 
     # Simulate
     scores = [100, 200, 300, -50, -100, -200, -300, -400, -500, -600, 400, 400, 400, 400, 400, 400, 400]
+    player_id = 1
+    station_id = 1
     for i, score in enumerate(scores):
-        await gl.attack_station(1, 1, score, time_base + timedelta(seconds=i + 0.5))
+        await gl.attack_station(player_id, station_id, score, time_base + timedelta(seconds=i + 0.5))
 
     # Test
     total_score = 0
@@ -124,12 +238,12 @@ async def test_station_score_history():
         elif total_score < 0:
             total_score = min(0, total_score + const.STATION_SCORE_DECAY_AMOUNT)
 
-        assert total_score == await gl.get_station_score(1, time_base + timedelta(seconds=i + eps))
+        assert total_score == await gl.get_station_score(station_id, time_base + timedelta(seconds=i + eps))
 
         # add the score
         total_score = clamp(total_score + scores[i], const.STATION_SCORE_LB, const.STATION_SCORE_UB)
 
-        assert total_score == await gl.get_station_score(1, time_base + timedelta(seconds=i + 0.5 + eps))
+        assert total_score == await gl.get_station_score(station_id, time_base + timedelta(seconds=i + 0.5 + eps))
 
     for i in range(len(scores), len(scores) + 10):
         # decay the score
@@ -138,8 +252,53 @@ async def test_station_score_history():
         elif total_score < 0:
             total_score = min(0, total_score + const.STATION_SCORE_DECAY_AMOUNT)
 
-        assert total_score == await gl.get_station_score(1, time_base + timedelta(seconds=i + eps))
+        assert total_score == await gl.get_station_score(station_id, time_base + timedelta(seconds=i + eps))
+
+
+async def test_game_score_history():
+    const.reset()
+    const.STATION_SCORE_DECAY_INTERVAL = 1
+    eps = 0.1
+
+    time_base = datetime.now()
+    gl = _GameLogic(pymongo.AsyncMongoClient("mongodb://localhost:27017"), time_base)
+    await gl.clear_database()
+
+    table = {
+        GameType.SHAKE_BADGE: [10, 15],
+        GameType.DINO: [20, 25],
+        GameType.SNAKE: [30, 35],
+        GameType.TETRIS: [40, 45],
+        GameType.CONNECT_SPONSOR: [50, 55],
+        GameType.RECTF: [60, 65],
+    }
+    player_id = 1
+    station_id = 1
+
+    # Simulate
+    for game_type, scores in table.items():
+        await gl.receive_game_score(player_id, station_id, scores[0], game_type, time_base + timedelta(seconds=0.5))
+        await gl.receive_game_score(player_id, station_id, scores[1], game_type, time_base + timedelta(seconds=1.5))
+
+    # Test
+    for game_type, scores in table.items():
+        assert 0 == await gl.get_game_score(player_id=player_id, station_id=station_id, game_type=game_type, before=time_base + timedelta(seconds=0 + eps))
+        assert scores[0] == await gl.get_game_score(player_id=player_id, station_id=station_id, game_type=game_type, before=time_base + timedelta(seconds=1 + eps))
+        assert scores[0] + scores[1] == await gl.get_game_score(player_id=player_id, station_id=station_id, game_type=game_type, before=time_base + timedelta(seconds=2 + eps))
+        assert scores[0] + scores[1] == await gl.get_game_score(player_id=player_id, station_id=station_id, game_type=game_type, before=time_base + timedelta(seconds=3 + eps))
+
+    assert await gl.get_game_score(player_id=player_id, before=time_base + timedelta(seconds=0 + eps)) == 0
+    assert await gl.get_game_score(player_id=player_id, before=time_base + timedelta(seconds=1 + eps)) == sum([
+        scores[0] for _, scores in table.items()
+    ])
+    assert await gl.get_game_score(player_id=player_id, before=time_base + timedelta(seconds=2 + eps)) == sum([
+        scores[0] + scores[1] for _, scores in table.items()
+    ])
+    assert await gl.get_game_score(player_id=player_id, before=time_base + timedelta(seconds=3 + eps)) == sum([
+        scores[0] + scores[1] for _, scores in table.items()
+    ])
 
 
 if __name__ == "__main__":
-    asyncio.run(test_station_score_history())
+    asyncio.run(test_attack_station_score_history())
+    asyncio.run(test_game_score_history())
