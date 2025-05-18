1c460755e2437948b4e5dafa1ec4ab81a072c540
Tony Yang <t510599@gmail.com>
2025-05-18 20:50:14 +0800

    fix(backend): event handler registration

--- backend/game_logic_controller.py
+++ backend/game_logic_controller.py
@@ -15,30 +15,36 @@ class GameLogic:
 
     # ===== APIs for PacketProcessor =====
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_single_badge_activity_event(evt: SingleBadgeActivityEvent):
         pass
 
 
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_two_badge_activity_event(evt: TwoBadgeActivityEvent):
         pass
 
 
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_game_activity_event(evt: GameActivityEvent):
         pass
 
 
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_sponsor_activity_event(evt: SponsorActivityEvent):
         pass
 
 
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_proximity_event(evt: ProximityEvent):
         pass
 
 
     @PacketProcessor.event_handler
+    @staticmethod
     async def on_pub_announce_event(evt: PubAnnounceEvent):
         pass
